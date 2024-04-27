ebceabe789d2a50a64fa750f7ba6b05fcf97e2bc
Aftab2522 <168265962+Aftab2522@users.noreply.github.com>
2024-04-27 15:21:31 +0500

    Update YT-search.js

--- commands/YT-search.js
+++ commands/YT-search.js
@@ -42,7 +42,7 @@ france({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk,
     for (let i = 0; i < 10; i++) {
       captions += `----------------\nTitle: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
     }
-    captions += "\n======\n*powered by AFTAB-4T4*";
+    captions += "\n======\n*powered by 🎗️AFTAB ABBASI🎗️*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
